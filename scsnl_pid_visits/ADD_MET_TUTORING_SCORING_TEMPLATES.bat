edd32775a2e380d212091c0068caaddfb6dbd44f
daelsaid <daelsaid@gmail.com>
2019-03-14 09:25:56 -0700

    commented code

--- scsnl_pid_visits/ADD_MET_TUTORING_SCORING_TEMPLATES.bat
+++ scsnl_pid_visits/ADD_MET_TUTORING_SCORING_TEMPLATES.bat
@@ -1,31 +1,47 @@
 @echo off
 
+:: daelsaid 03142019
+::------------------------
+
+::script assumes MET visit structure
+::PID_VISIT suffix to replace template in scoring files set to visit 1
+::------------------------
+
+::paths to adjust below
+::NP parent folder
+::Folder with tutoring templates
+::output
 set parent_dir="C:\Users\daelsaid\Desktop\np_testing"
 set main_tutoring_template_dir="%parent_dir%\np"
 set np_subj_data_path="%parent_dir%\output"
 
+::do not change below, all paths are relative to above
+::user entered prompts
 set /p pid="Enter Subject's PID only (####):"
 set /p tutoring_group="Enter Subject's tutoring group (spt or vnt):"
 
+ ::tutoring specific template folder
 set tutoring_group_template_dir="%main_tutoring_template_dir%\tutoring_%tutoring_group%"
 set main_subj_dir="%np_subj_data_path%\%pid%"
 set visit_dir="%main_subj_dir%\visit1"
 set lab_expir="%visit_dir%\assessments\lab_experiments"
-set file_ending="%pid%_1"
 set tutoring_dir="%lab_expir%\tutoring"
+set file_ending="%pid%_1"
 
-
+:: if tutoring folder already exists, skip commands and end
 if exist "%tutoring_dir%" GOTO :tutoring_dir_exists
+
+::check if a tutoring does not exist and make tutoring folder in PID/visit1/assessments/lab_experiments
+::copy group specific templates into PID/visit1/assessments/lab_experiments/tutoring
 
 if not exist "%tutoring_dir%" ECHO CREATING TUTORING FOLDER FOR %pid%
 cd %lab_expir%
 md "tutoring"
 xcopy %tutoring_group_template_dir% %lab_expir%\tutoring
-
-
 cd %tutoring_dir%
+::replace template suffix with PID_VISIT file ending
 rename "*_template.*" "*_%file_ending%.*"
 
 :tutoring_dir_exists
-ECHO TUTORING DIR EXISTS
+ECHO TUTORING DIR ALREADY EXISTS
 :end
--- scsnl_pid_visits/CREATE_SCSNL_PID_FOLDER.bat
+++ scsnl_pid_visits/CREATE_SCSNL_PID_FOLDER.bat
@@ -1,11 +1,27 @@
 @echo off
 
+::daelsaid 03142019
+::------------------------
+
+::paths to adjust below
+::NP parent folder
+::template folder
+:: output
 set parent_dir="C:\Users\daelsaid\Desktop\np_testing"
 set np_fldr_template_path="%parent_dir%\np\np_template_folder"
 set np_subj_data_path="%parent_dir%\output"
 
+::do not change below, all paths are relative to above
+
+::user entered prompts
+::numerical PID ONLY (9999)
+::numerical visit number ONLY (1)
 set /p pid="Enter Subject's PID only(####):"
 set /p visit="Enter Subject's Visit Number (#):"
+
+::the following folder structure represents the SCSNL PID-VISIT-ASSESSMENTS folder structure
+:: set main subject folder
+::set visit specific folder based off of user entered value
 
 set main_subj_dir="%np_subj_data_path%\%pid%"
 set visit_dir="%main_subj_dir%\visit%visit%"
@@ -15,18 +31,24 @@ set questionnaires="%lab%\questionnaire"
 set stand="%visit_dir%\assessments\standardized_experiments"
 set file_ending="%pid%_%visit%"
 
+::if visit does not equal 1, begin runinng lines at visit not initial
 if "%visit%" NEQ "1" GOTO :visit_not_initial
 
+::If PID folder already exists, skip the lines until you reach rename
+:: if PID does not exist, create PID folder and copy tmeplate folder structure + scoring templates into PID
 if not exist "%main_subj_dir%" ECHO PID FOLDER DOES NOT EXIST, CREATING PID FOLDER WITH TEMPLATES
 md "%main_subj_dir%"
 xcopy %np_fldr_template_path% %np_subj_data_path%\%pid% /E
 GOTO :rename_files
 
+::assumes that the folder has been created
 :visit_not_initial
-ECHO PID FOLDER EXISTS, RENAMING VISIT 2 TEMPLATES
+ECHO PID FOLDER EXISTS, REMOVING template SUFFIX FOR VISIT ENTERED
 GOTO :rename_files
 
 :rename_files
+:: change directories for each relevant subfolder and replaces _template suffix with PID_VISIT
+
 cd %lab%
 rename "*_template.*" "*_%file_ending%.*"
 
