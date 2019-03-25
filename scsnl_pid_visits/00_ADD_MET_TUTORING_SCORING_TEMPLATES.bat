3309dd2bf65bf82820eb426b8e4fb7d854ecac5b
daelsaid <daelsaid@stanford.edu>
2019-03-25 12:15:24 -0700

    updated to work on NP computers

--- scsnl_pid_visits/00_ADD_MET_TUTORING_SCORING_TEMPLATES.bat
+++ scsnl_pid_visits/00_ADD_MET_TUTORING_SCORING_TEMPLATES.bat
@@ -1,4 +1,5 @@
-@echo off
+@echo off0
+pushd "%~dp0"
 
 :: daelsaid 03142019
 ::------------------------
@@ -11,14 +12,15 @@
 ::NP parent folder
 ::Folder with tutoring templates
 ::output
-set parent_dir="C:\Users\daelsaid\Desktop\np_testing"
-set main_tutoring_template_dir="%parent_dir%\np"
-set np_subj_data_path="%parent_dir%\output"
-
+set parent_dir=\\171.65.52.100\NeuroPsych
+set np_subj_data_path=%parent_dir%
+set project=met
 ::do not change below, all paths are relative to above
 ::user entered prompts
 set /p pid="Enter Subject's PID only (####):"
 set /p tutoring_group="Enter Subject's tutoring group (spt or vnt):"
+
+set main_tutoring_template_dir=%parent_dir%\project_template_folders\%project%
 
 ECHO Please confirm that you have entered the correct information
 
@@ -27,12 +29,12 @@ ECHO Tutoring Group: %tutoring_group%
 pause
 
  ::tutoring specific template folder
-set tutoring_group_template_dir="%main_tutoring_template_dir%\%project%\%project%_np_template_folder\tutoring_%tutoring_group%"
-set main_subj_dir="%np_subj_data_path%\%pid%"
-set visit_dir="%main_subj_dir%\visit1"
-set lab_expir="%visit_dir%\assessments\lab_experiments"
-set tutoring_dir="%lab_expir%\tutoring"
-set file_ending="%pid%_1"
+set tutoring_group_template_dir=%main_tutoring_template_dir%\tutoring_%tutoring_group%
+set main_subj_dir=%np_subj_data_path%\%pid%
+set visit_dir=%main_subj_dir%\visit1
+set lab_expir=%visit_dir%\assessments\lab_experiments
+set tutoring_dir=%lab_expir%\tutoring
+set file_ending=%pid%_1
 
 :: if tutoring folder already exists, skip commands and end
 if exist "%tutoring_dir%" GOTO :tutoring_dir_exists
@@ -41,13 +43,15 @@ if exist "%tutoring_dir%" GOTO :tutoring_dir_exists
 ::copy group specific templates into PID/visit1/assessments/lab_experiments/tutoring
 
 if not exist "%tutoring_dir%" ECHO CREATING TUTORING FOLDER FOR %pid%
-cd %lab_expir%
-md "tutoring"
-xcopy %tutoring_group_template_dir% %lab_expir%\tutoring
-cd %tutoring_dir%
+md "%lab_expir%\tutoring"
+xcopy %tutoring_group_template_dir% "%lab_expir%\tutoring" /E
+
 ::replace template suffix with PID_VISIT file ending
-rename "*_template.*" "*_%file_ending%.*"
+ECHO RENAMING TUTORING TEMPLATES
+ren %lab_expir%\tutoring\*_template.* *_%file_ending%.*
 
 :tutoring_dir_exists
 ECHO TUTORING DIR ALREADY EXISTS
+ECHO "Please check tutoring folder!"
+pause
 :end
--- scsnl_pid_visits/0_CREATE_SCSNL_PID_FOLDER.bat
+++ scsnl_pid_visits/0_CREATE_SCSNL_PID_FOLDER.bat
@@ -1,4 +1,5 @@
 @echo off
+pushd "%~dp0"
 
 ::daelsaid 03142019
 ::------------------------
@@ -7,9 +8,10 @@
 ::NP parent folder
 ::template folder
 :: output
-set parent_dir="%HOMEPATH%\Desktop\np_testing"
-set np_subj_data_path="%parent_dir%\output"
 
+set parent_dir=\\171.65.52.100\NeuroPsych
+set np_subj_data_path=%parent_dir%
+set np_comp_address=\\171.65.52.100
 ::do not change below, all paths are relative to above
 
 ::user entered prompts
@@ -25,18 +27,19 @@ ECHO VISIT: %visit%
 ECHO PROJECT: %project%
 pause
 
-set np_fldr_template_path="C:\Users\daelsaid\Desktop\np_testing\np\%project%\%project%_np_template_folder"
+set np_fldr_template_path=%parent_dir%\project_template_folders\%project%\%project%_np_template_folder
 
 ::the following folder structure represents the SCSNL PID-VISIT-ASSESSMENTS folder structure
 :: set main subject folder
 ::set visit specific folder based off of user entered value
-set main_subj_dir="%np_subj_data_path%\%pid%"
-set visit_dir="%main_subj_dir%\visit%visit%"
-set lab="%visit_dir%\assessments\lab_experiments"
-set sa="%lab%\strategy_assessment"
-set questionnaires="%lab%\questionnaire"
-set stand="%visit_dir%\assessments\standardized_experiments"
-set file_ending="%pid%_%visit%"
+set main_subj_dir=%np_subj_data_path%\%pid%
+set visit_dir=%main_subj_dir%\visit%visit%
+set lab=%visit_dir%\assessments\lab_experiments
+set sa=%lab%\strategy_assessment
+set questionnaires=%lab%\questionnaire
+set stand=%visit_dir%\assessments\standardized_experiments
+set file_ending=%pid%_%visit%
+
 
 ::if visit does not equal 1, begin runinng lines at visit not initial
 if "%visit%" NEQ "1" GOTO :visit_not_initial
@@ -45,7 +48,7 @@ if "%visit%" NEQ "1" GOTO :visit_not_initial
 :: if PID does not exist, create PID folder and copy tmeplate folder structure + scoring templates into PID
 if not exist "%main_subj_dir%" ECHO PID FOLDER DOES NOT EXIST, CREATING PID FOLDER WITH TEMPLATES
 md "%main_subj_dir%\visit%visit%"
-xcopy %np_fldr_template_path%\visit%visit% %np_subj_data_path%\%pid%\visit%visit%\ /E
+xcopy %np_fldr_template_path%\visit%visit% %main_subj_dir%\visit%visit% /E
 GOTO :rename_files
 
 ::assumes that the folder has been created
@@ -56,20 +59,14 @@ md "%main_subj_dir%\visit%visit%"
 xcopy %np_fldr_template_path%\visit%visit% %np_subj_data_path%\%pid%\visit%visit% /E
 GOTO :rename_files
 
-
-
 :rename_files
 :: change directories for each relevant subfolder and replaces _template suffix with PID_VISIT
 
-cd %lab%
-rename "*_template.*" "*_%file_ending%.*"
+ren %lab%\*_template*.* *_%file_ending%.*
 
-cd %sa%
-rename "*_template.*" "*_%file_ending%.*"
+ren %sa%\*_template*. *_%file_ending%.*
 
-cd %questionnaires%
-rename "*_template.*" "*_%file_ending%.*"
+ren %questionnaires%\*_template*. *_%file_ending%.*
 
-cd %stand%
-rename "*_template.*" "*_%file_ending%.*"
+ren %stand%\*_template*. *_%file_ending%.*
 :end
